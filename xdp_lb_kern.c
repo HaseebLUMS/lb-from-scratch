3c9e5a7477a7baa6689a65c5c722eb2f5bf6699a
Muhammad Haseeb <mh6218@nyu.edu>
2023-05-08 18:16:42 -0400

    udapted

--- xdp_lb_kern.c
+++ xdp_lb_kern.c
@@ -46,7 +46,7 @@ int xdp_load_balancer(struct xdp_md *ctx)
         // char be = BACKEND_A;
         // if (bpf_ktime_get_ns() % 2)
         //     be = BACKEND_B;
-
+        bpf_printk("Hit if branch\n");
         iph->daddr = RECP_IP1;
         
         eth->h_dest[0] = recp_mac1[0];
@@ -59,13 +59,22 @@ int xdp_load_balancer(struct xdp_md *ctx)
     else
     {
         bpf_printk("Hit else branch\n");
-        iph->daddr = CLIENT_IP;
-        eth->h_dest[0] = client_mac[0];
-        eth->h_dest[1] = client_mac[1];
-        eth->h_dest[2] = client_mac[2];
-        eth->h_dest[3] = client_mac[3];
-        eth->h_dest[4] = client_mac[4];
-        eth->h_dest[5] = client_mac[5];
+        iph->daddr = RECP_IP1;
+        
+        eth->h_dest[0] = recp_mac1[0];
+        eth->h_dest[1] = recp_mac1[1];
+        eth->h_dest[2] = recp_mac1[2];
+        eth->h_dest[3] = recp_mac1[3];
+        eth->h_dest[4] = recp_mac1[4];
+        eth->h_dest[5] = recp_mac1[5];
+        
+        // iph->daddr = CLIENT_IP;
+        // eth->h_dest[0] = client_mac[0];
+        // eth->h_dest[1] = client_mac[1];
+        // eth->h_dest[2] = client_mac[2];
+        // eth->h_dest[3] = client_mac[3];
+        // eth->h_dest[4] = client_mac[4];
+        // eth->h_dest[5] = client_mac[5];
     }
     iph->saddr = PROXY_IP;
     eth->h_source[0] = proxy_mac[0];
